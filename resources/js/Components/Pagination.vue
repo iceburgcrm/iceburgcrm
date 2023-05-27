45ada7b9949947bf0935906ac73102610ec03e28
htmujahid <htmujahid@gmail.com>
2023-05-27 19:26:58 +0500

    removed unnecessary scrollbars

--- resources/js/Components/Pagination.vue
+++ resources/js/Components/Pagination.vue
@@ -1,6 +1,6 @@
 <template>
 <div v-if="links.length > 3">
-<div class="flex flex-wrap -mb-1">
+<div class="flex flex-wrap mb-1">
     <template v-for="(link, p) in props.links" :key="p">
         <div v-if="p == props.page" class="mr-1 mb-1 px-4 py-3 text-sm leading-4 text-primary-content border rounded"
              v-html="link.label" :class="{ 'bg-primary text-base-100': link.active }"  />
