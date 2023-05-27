7ef07d716bdb3b25acf2025005078851cfbdd1c4
htmujahid <htmujahid@gmail.com>
2023-05-27 16:27:29 +0500

    make whole show search button clickable

--- resources/js/Pages/Module/List.vue
+++ resources/js/Pages/Module/List.vue
@@ -23,15 +23,15 @@
 
 
                 <div tabindex='0' class="collapse">
-                    <input type="checkbox" v-model="show_search"  />
-                    <div class="collapse-title text-xsm">
+                    <input type="checkbox" v-model="show_search" id="show-search" />
+                    <label class="collapse-title text-xsm select-none" for="show-search">
 
                         <span v-if="!show_search">Show</span><span v-if="show_search">Hide</span> Search Fields
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 13.5L12 21m0 0l-7.5-7.5M12 21V3" />
                     </svg>
 
-                    </div>
+                    </label>
 
                 <div class="collapse-content">
                     <form name="search" method="get">
